6c4188e8225340bfddaab102b9de530d60060ef3
arma7x <arma7x@live.com>
2021-10-25 19:08:42 +0800

    update

--- Makefile
+++ Makefile
@@ -2,14 +2,15 @@ COMP = gcc
 FLAGS = -std=c99 -g3 -Wall
 SRC = *.c
 OBJ = *.o
+DEPS = header.h
 BIN = HostsMod
 
 $(BIN) : $(OBJ)
 	$(COMP) $(FLAGS) $(OBJ) -o $(BIN)
 	rm $(OBJ)
-$(OBJ) : $(SRC)
+$(OBJ) : $(SRC) $(DEPS)
 	$(COMP) $(FLAGS) -c $(SRC)
-clean : 
-	rm $(BIN) $(OBJ)
+clean :
+	rm $(OBJ)
 windows :
 	$(COMP) main.c -o $(BIN)
--- main.c
+++ main.c
@@ -1,82 +1,108 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
-//Really just is not windows
-//int isPosixComplient;
-
-#ifdef _WIN32
-int isPosixComplient = 0;
-#else
-int isPosixComplient = 1;
-#endif
-
-void *err_malloc(size_t size);
-void err_free(void *ptr);
-void mod_hosts();
+#include "header.h"
 
 int main(int argc, char *argv[])
 {
+    if (argc == 1) {
+      printf("Missing block parameter: HostsMod /path/to/block/list\n");
+      exit(EXIT_FAILURE);
+    }
+
+    int total_b = 0;
+    Lines **blocks = extract_lines(argv[argc-1], &total_b, NULL);
+
+    int total_h = 0;
+    Lines **hosts = extract_lines("/etc/hosts\0", &total_h, NULL);
+
     char *path;
-    if(isPosixComplient)
-    {
-        char src[] = "/etc/hosts\0";
-        short length = (short)strlen(src);
-        path = (char *)err_malloc(length);
-        strncpy(path, src, length);
-    }
-    else
-    {
-        char src[] = "C:\\Windows\\System32\\drivers\\etc\\hosts\0";
-        short length = (short)strlen(src);
-        path = (char *)err_malloc(length);
-        strncpy(path, src, length);
-    }
-    FILE *file_ptr = fopen(path, "a");
-    mod_hosts(file_ptr); 
+    char src[] = "/etc/hosts\0";
+    short length = (short)strlen(src);
+    path = (char *)err_malloc(length);
+    path[length] = '\0';
+    strncpy(path, src, length);
+    FILE *file_ptr = fopen(path, "w");
+    mod_hosts(file_ptr, hosts, total_h, blocks, total_b); 
     fclose(file_ptr);
-    printf("Wrote to %s\n", path);
     err_free(path);
+    printf("DONE\n");
     return 0;
+}
+
+Lines **extract_lines(char *path, int *n, char *stop)
+{
+  Lines **list = (Lines**) malloc(0 * sizeof(Lines*));
+  FILE *fptr;
+  char * line = NULL;
+  size_t len = 0;
+  size_t read;
+  fptr = fopen(path,"rb");
+  int nth = 0;
+  while ((read = getline(&line, &len, fptr)) != -1)
+  {
+    if (read - 1 > 0) {
+      line[read - 1] = '\0';
+      char *ret = strstr(line, "#KAIOS");
+      if (ret != NULL)
+        break;
+      list = (Lines**) realloc(list, (nth + 1) * sizeof(Lines*));
+      list[nth] = (Lines*) malloc(sizeof(Lines));
+      (*list[nth]).text = (char*) malloc(read * sizeof(char));
+      strncpy((*list[nth]).text, line, read - 1);
+      (*list[nth]).len = read - 1;
+      nth++;
+    }
+  }
+  fclose(fptr);
+  if (line)
+    free(line);
+  *n = nth;
+  return list;
 }
 
 void *err_malloc(size_t size)
 {
-    void *ptr = malloc(size);
-    if(ptr == NULL)
-    {
-        perror("Couldn't allocate memory");
-        exit(-1);
-    }
-    return ptr;
+  void *ptr = malloc(size);
+  if(ptr == NULL)
+  {
+    perror("Couldn't allocate memory");
+    exit(-1);
+  }
+  return ptr;
 }
+
 void err_free(void *ptr)
 {
-    if(ptr == NULL)
-    {
-        return;
-    }
-    free(ptr);
-    ptr = NULL;
+  if(ptr == NULL)
+  {
+      return;
+  }
+  free(ptr);
+  ptr = NULL;
 }
-void mod_hosts(FILE *file_ptr)
+
+void mod_hosts(FILE *file_ptr, Lines **orig, int no, Lines **block, int nb)
 {
-    char comment[] = "\n\n#These sites were blocked from you for a reason, please don't unblock them\n\n\0";
-    fwrite(comment, sizeof(char), (short)strlen(comment), file_ptr);
-    char loopback[] = "127.0.0.1\t";
-    short loopback_length = (short)strlen(loopback);
-    const char *sites[] = {"www.pornhub.com", "www.rule34.xxx", "www.xvideos.com", "www.nehentai.net", "www.hanime.tv", "www.xhamster.com", "www.hentaihaven.com", "www.redtube.com", "www.chaturbate.com", "www.livejasmin.com", "www.slutroulette.com", "www.onlyfans.com", "www.8tube.xxx", "www.youjizz.com", "www.hentaihaven.xxx", NULL};
-    //We need a part 2 in order to block non wwww versions
-    const char *sites2[] = {"pornhub.com", "rule34.xxx", "xvideos.com", "nehentai.net", "hanime.tv", "xhamster.com", "hentaihaven.com", "redtube.com", "chaturbate.com", "livejasmin.com", "slutroulette.com", "onlyfans.com", "8tube.xxx", "youjizz.com", "hentaihaven.xxx", NULL};
-    int i = 0;
-    while(sites[i] != NULL)
-    {
-        fwrite(loopback, sizeof(char), (int)loopback_length, file_ptr);
-        fwrite(sites[i], sizeof(char), strlen(sites[i]), file_ptr);
-        fwrite("\n", sizeof(char), 1, file_ptr);
-        fwrite(loopback, sizeof(char), (int)loopback_length, file_ptr);
-        fwrite(sites2[i], sizeof(char), strlen(sites2[i]), file_ptr);
-        fwrite("\n", sizeof(char), 1, file_ptr);
-        ++i;
-    }
+  for (int x=0;x<no;x++)
+  {
+    fwrite(orig[x]->text, sizeof(char), orig[x]->len, file_ptr);
+    fwrite("\n", sizeof(char), 1, file_ptr);
+    free(orig[x]);
+  }
+  free(orig);
+
+  char comment[] = "\n#KAIOS\n";
+  fwrite(comment, sizeof(char), (short)strlen(comment), file_ptr);
+  
+  char loopback[] = "0.0.0.0 ";
+  short loopback_length = (short)strlen(loopback);
+  for (int x=0;x<nb;x++)
+  {
+    fwrite(loopback, sizeof(char), (int)loopback_length, file_ptr);
+    fwrite(block[x]->text, sizeof(char), block[x]->len, file_ptr);
+    fwrite("\n", sizeof(char), 1, file_ptr);
+    free(block[x]);
+  }
+  free(block);
 }
